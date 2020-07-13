4a9871dc67d35b2c8b3f6b1b0134706dabb0a08a
Paola <camachopaola311@gmail.com>
2020-07-13 11:27:58 -0400

    check in

--- src/main/java/com/stayready/assessment/week2/part01/PredicateUtilities.java
+++ src/main/java/com/stayready/assessment/week2/part01/PredicateUtilities.java
@@ -6,7 +6,10 @@ public class PredicateUtilities {
      * @return true if `value` is a multiple of 2
      */
     public static Boolean isEven(Integer value) {
-        return null;
+        if(value % 2 == 0){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -14,7 +17,7 @@ public class PredicateUtilities {
      * @return true if `value` is not a multiple of 2
      */
     public static Boolean isOdd(Integer value) {
-        return null;
+        return !isEven(value);
     }
 
     /**
@@ -22,7 +25,10 @@ public class PredicateUtilities {
      * @return true if `value` is a multiple of 3
      */
     public static Boolean isMultipleOf3(Integer value) {
-        return null;
+        if(value % 3 == 0){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -32,7 +38,10 @@ public class PredicateUtilities {
      * @return true if `value` is a multiple of `multiple`
      */
     public static Boolean isMultipleOfN(Integer value, Integer multiple) {
-        return null;
+        if(value % multiple == 0){
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -40,6 +49,11 @@ public class PredicateUtilities {
      * @return true if `string` starts with a capital letter
      */
     public static Boolean startsWithCapitalLetter(String string) {
-        return null;
+        boolean bool = false;
+        if(string.matches("[a-z]") || string.matches("[A-Z]")){
+            bool = true;
+        }
+        return bool;
+    
     }
 }
