09cba40aed8e0c91bfc3ce6bf4ed9e650bf6d16a
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-13 19:41:20 -0500

    added some more

--- src/main/java/com/stayready/assessment/week2/part05/Palindrome.java
+++ src/main/java/com/stayready/assessment/week2/part05/Palindrome.java
@@ -1,11 +1,14 @@
 package com.stayready.assessment.week2.part05;
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class Palindrome {
     public Integer countPalindromes(String input){
         ArrayList<String> arrList = new ArrayList<String>();
+        String firstHalf = "";
+        StringBuilder secondHalf = new StringBuilder();
+        int length = 0;
+        //generating all of the different ways the string can be arranged
         for(int i = 0; i < input.length(); i++) {
             for(int j = i + 1; j <= input.length(); j++) {
                 arrList.add(input.substring(i, j));
@@ -13,32 +16,27 @@ public class Palindrome {
         }
         Integer numPalindromes = 0;
         for(String word: arrList) {
-            int length = word.length();
+            length = word.length();
             if(length == 1) {
                 numPalindromes++;
             }
             else if(length == 2) {
-                char[] individualLetters = word.toCharArray();
-                if(individualLetters[0] == individualLetters[1]) {
+                if(word.substring(0, 1).equals(word.substring(1, 2))) {
                     numPalindromes++;
                 }
             }
             else if(length > 2) {
+                firstHalf = word.substring(0, length / 2);
+                //clearing the contents
+                secondHalf.setLength(0);
                 if(length % 2 == 0) {
-                    String firstHalf = word.substring(0, length / 2);
-                    StringBuilder secondHalf = new StringBuilder();
                     secondHalf.append(word.substring(length/2)).reverse();
-                    if(firstHalf.equals(secondHalf.toString())) {
-                        numPalindromes++;
-                    }
                 }
                 else if(length % 2 == 1){
-                    String firstHalf = word.substring(0, length / 2);
-                    StringBuilder secondHalf = new StringBuilder();
                     secondHalf.append(word.substring((length/2) + 1)).reverse();
-                    if(firstHalf.equals(secondHalf.toString())) {
-                        numPalindromes++;
-                    }
+                }
+                if(firstHalf.equals(secondHalf.toString())) {
+                    numPalindromes++;
                 }
             }
         }
